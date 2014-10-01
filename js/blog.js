a6912afc92965114b0d7f755ee80ad69cbd2ee08
khairold <khairold@gmail.com>
2014-10-01 19:50:28 +0800

    Second Commit

---
+++ js/blog.js
@@ -0,0 +1,25 @@
+$(function() {
+
+    Parse.$ = jQuery;
+
+	// Replace this line with the one on your Quickstart Guide Page
+	Parse.initialize("bHiD8Rb40AiYtkBiXh66v6kryTCmkF9YSoeo7XMh", "EMEaSJGwF8XJq2QPQ1sboY8MC7QxO9L6HyvhlsRH");
+
+	var Blog = Parse.Object.extend("Blog");
+    
+    var Blogs = Parse.Collection.extend({
+        model: Blog
+    });
+    
+    var blogs = new Blogs();
+    
+    blogs.fetch({
+	   success: function(blogs) {
+           console.log(blogs);
+	   },
+	   error: function(blogs, error) {
+           console.log(error);
+	   }
+    });
+
+});
